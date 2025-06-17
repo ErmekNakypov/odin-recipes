8656aacb1effb40f832ce170c57639148da82625
spacermek <spacermek@gmail.com>
2025-06-17 22:40:19 +0600

    new changes with dom

--- scrips.js
+++ scrips.js
@@ -1,50 +1,68 @@
-function playGame() {
-    let computerScore = 0;
-    let humanScore = 0; 
-    const rules = {
-        rock: "scissors",
-        paper: "rock",
-        scissors: "paper"
-    };
+let computerScore = 0;
+let humanScore = 0; 
+const rules = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+};
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        console.log("Human: " + humanChoice);
-        console.log("Computer: " + computerChoice);
-
-        if (humanChoice === computerChoice) {
-            console.log("Draw");
-        } 
-        else if (rules[humanChoice] === computerChoice) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        } 
-        else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-
-        console.log("\n");
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection)
-    }
-
-    console.log("Computer won: " + computerScore)
-    console.log("Human won: " + humanScore)
-    if (computerScore > humanScore) {
-        console.log("Overall computer won");
-    }
-    else if (humanScore > computerScore) {
-         console.log("Overall human won");
-    }
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    if (humanChoice === computerChoice) {
+        gameState.textContent += 'Draw\n'
+    } 
+    else if (rules[humanChoice] === computerChoice) {
+        gameState.textContent += `You win! ${humanChoice} beats ${computerChoice}\n`
+        humanScore++;
+    } 
     else {
-        console.log("Draw")
+        gameState.textContent += `You lose! ${computerChoice} beats ${humanChoice}\n`
+        computerScore++;
+    }
+
+    scores.textContent = 'Your score: ' + humanScore + '\n'
+    scores.textContent += 'Computer score: ' + computerScore + '\n\n'
+
+    if (computerScore == 5 || humanScore == 5) {
+        computerScore = 0
+        humanScore = 0
+        gameState.textContent = ''
+        if (computerScore > humanScore) {
+            gameState.textContent += 'Overall: computer won'
+        }
+        else if (humanScore > computerScore) {
+            gameState.textContent += 'Overall: human won'
+        }
+        else {
+            gameState.textContent += 'Overall: draw'
+        }
     }
 }
+
+    const results = document.querySelector('.results')
+    const gameState = document.createElement('p')
+    const scores = document.createElement('p')
+
+    scores.textContent = 'Your score: ' + humanScore + '\n'
+    scores.textContent += 'Computer score: ' + computerScore + '\n\n'
+
+    results.appendChild(scores)
+    results.appendChild(gameState)
+
+    results.setAttribute('style', 'white-space: pre-line; background-color: pink; margin-top: 10px;')
+
+    const container = document.querySelector('.container')
+
+    container.addEventListener('click', (e) => {
+        const computerSelection = getComputerChoice()
+        const content = e.target.textContent
+        gameState.textContent = ''
+        gameState.textContent += `You: ${content}\n`
+        gameState.textContent += 'Computer: ' + computerSelection + '\n'
+        playRound(content, computerSelection)
+    })
+  
 
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
@@ -59,10 +77,3 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
-function getHumanChoice() {
-    let input = prompt("Enter:");
-    return input
-}
-
-playGame()
